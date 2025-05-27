72cbae79d4449731200d4bffdbfbd1c400d7ec51
Gautham V <gauthamvr2005@gmail.com>
2025-05-27 10:09:48 +0530

    217

---
+++ 217. Contains Duplicate.c
@@ -0,0 +1,14 @@
+int compare(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
+
+bool containsDuplicate(int* nums, int numsSize) {
+    if (numsSize <= 1) {
+        return false;
+    }
+    qsort(nums, numsSize, sizeof(int), compare);
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] == nums[i - 1]) {
+            return true;
+        }
+    }
+    return false;
+}
