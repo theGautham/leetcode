6d8d2fdd9cfec90ff580a0abc95a18297d1161ef
Gautham V <gauthamvr2005@gmail.com>
2025-05-30 17:53:57 +0530

    2011

---
+++ 2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        X=0
+        for op in operations:
+            if op == "X++" or op== "++X":
+                X += 1
+            elif op == "--X" or op== "X--":
+                X -= 1
+        return X
+        
+        
