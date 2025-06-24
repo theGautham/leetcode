841ef731d1e7a683702229fbd798ab1d7137f960
Gautham V <gauthamvr2005@gmail.com>
2025-06-24 23:52:23 +0530

    2341

--- 771. Jewels and Stones.py
+++ 771. Jewels and Stones.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
-        jewels_set = set(jewels)
-        return sum(s in jewels_set for s in stones)
+        jewel_set = set(jewels)
+        return sum(s in jewel_set for s in stones)
